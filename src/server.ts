91b922d100aa5d2dcd48301e8b9d014eccf920a8
kanon8888 <mdrakibhasankanon@gmail.com>
2026-05-21 22:07:04 +0600

    one commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-import express from "express"
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+const app: Application = express();
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', (req: Request, res: Response) => {
+    // res.send('Hello World!')
+    res.status(200).json({
+        "success": true,
+        "message": "DevPulse Server is running successfully",
+
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
