66a06d3b62807d70197ddb47da2da4f803f29861
kanon8888 <mdrakibhasankanon@gmail.com>
2026-05-22 08:49:14 +0600

    three commit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express, { type Application, type Request, type Response } from "express"
 const app: Application = express();
-const port = 5000
+const port = 5000;
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded);
 
 app.get('/', (req: Request, res: Response) => {
     // res.send('Hello World!')
